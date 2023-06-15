7d4f14f996323596937d300615e1fec7deb60c9d
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-06-15 14:56:51 +0200

    up

--- sources/exits/freeing.c
+++ sources/exits/freeing.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   freeing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/15 14:49:58 by mgama             #+#    #+#             */
-/*   Updated: 2023/06/15 14:56:09 by mgama            ###   ########.fr       */
+/*   Created: 2023/06/15 14:56:44 by mgama             #+#    #+#             */
+/*   Updated: 2023/06/15 14:56:50 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
-
-int	main(int argc, char *argv[], char *envp[])
-{
-	t_commands	commands;
-	
-	(void)(argc); // temp ignoring
-	(void)(argv); // temp ignoring
-	ft_bzero(&commands, sizeof(t_commands));
-	commands.envp = envp;
-	// parsing function
-	fork_processes(&commands);
-	return (0);
-}
+#include "../../includes/minishell.h"
