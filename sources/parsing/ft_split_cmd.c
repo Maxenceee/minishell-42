f2a679db8e8b60dc984d0a67b9e9a18d1781451a
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-11-03 16:42:12 +0100

    split cmd

--- sources/parsing/ft_split_cmd.c
+++ sources/parsing/ft_split_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:05:16 by mgama             #+#    #+#             */
-/*   Updated: 2023/11/03 16:38:39 by mgama            ###   ########.fr       */
+/*   Updated: 2023/11/03 16:41:56 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,8 @@ int	cmd_count(char *line)
 	j = 0;
 	read_env = 0;
 	quoted = 0;
-	printf("line: %s\n\n", line);
 	while (line[i])
 	{
-		printf("%d %c\n", i, line[i]);
 		if (!read_env && line[i] == '\'')
 			quoted = !quoted;
 		else if (!quoted && line[i] == '\"')
@@ -36,7 +34,7 @@ int	cmd_count(char *line)
 		if (!(quoted || read_env) && line[i])
 			j++;
 		while (line[i] && !(quoted || read_env) && line[i] != ' ')
-			i++; printf("%d (%c)\n", i, line[i]);
+			i++;
 	}
 	return (j);
 }
@@ -67,14 +65,11 @@ char	*put_cmd(t_data *minishell, char *line, int *i)
 	int		len;
 
 	len = get_cmd_len(line);
-	printf("get_cmd_len: %d\n", len);
 	res = ft_strtcpy(line, len);
-	printf("ft_strtcpy: %s\n", res);
 	if (!res)
 		return (NULL);
 	*i += len;
 	cmd = ft_parse_expands(minishell, res);
-	printf("ft_parse_expands: %s\n", cmd);
 	free(res);
 	return (cmd);
 }
