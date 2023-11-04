9d7aacc8bd98331c965e010249ccd3e1d0c12b5e
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-11-04 05:27:44 +0100

    exit todo

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,12 +6,18 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 14:58:24 by mgama             #+#    #+#             */
-/*   Updated: 2023/11/04 02:19:26 by mgama            ###   ########.fr       */
+/*   Updated: 2023/11/04 05:27:11 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * TODO:
+ * 
+ * Ajouter les argument de exit 
+ * 
+ */
 int	ft_builtin_exit(t_data *ms)
 {
 	write(2, "exit\n", 6);
