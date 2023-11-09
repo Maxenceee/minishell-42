41c694ed0caad6a8d6005739c556a12a3b035cb8
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-11-09 16:43:49 +0100

    dup leaks

--- sources/exits/freeing.c
+++ sources/exits/freeing.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 14:56:44 by mgama             #+#    #+#             */
-/*   Updated: 2023/11/09 16:15:53 by mgama            ###   ########.fr       */
+/*   Updated: 2023/11/09 16:43:22 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	free_minishell(t_data *minishell)
 {
 	free_env(minishell);
+	int	i = -1;
 	if (minishell->envp)
 		free_tab(minishell->envp);
 	if (minishell->parsing_cmd)
